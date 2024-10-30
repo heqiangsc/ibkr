95ce338314debdb0afd90b4c2b99c01a41d1b622
heqiang <heqiang@kuainiugroup.com>
2024-10-30 15:40:38 +0800

    yfinance

---
+++ backtrader/backtrader_v1.py
@@ -0,0 +1,40 @@
+import backtrader as bt
+import yfinance as yf
+
+
+# Create a subclass of bt.Strategy
+class TestStrategy(bt.Strategy):
+    def next(self):
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+
+
+# Create a cerebro entity
+cerebro = bt.Cerebro()
+
+# Add a strategy
+cerebro.addstrategy(TestStrategy)
+
+# Create a Data Feed
+
+
+data = bt.feeds.PandasData(dataname=yf.download("MSFT",
+                                                start="2024-06-01",
+                                                end="2026-10-20"))
+
+# Add the Data Feed to Cerebro
+cerebro.adddata(data)
+# Set our desired cash start
+cerebro.broker.setcash(100000.0)
+
+# Add a FixedSize sizer according to the stake
+cerebro.addsizer(bt.sizers.FixedSize, stake=10)
+
+# Set the commission
+cerebro.broker.setcommission(commission=0.001)
+
+# Run over everything
+cerebro.run()
+
+# Plot the result
+cerebro.plot(style='candle')
