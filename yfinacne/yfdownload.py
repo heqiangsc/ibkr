42bef8c7c3ad35974daf8f9cdcf21e7937c4eacb
heqiang <heqiang@kuainiugroup.com>
2024-10-30 15:32:26 +0800

    yfinance

--- yfinacne/yfdownload.py
+++ yfinacne/yfdownload.py
@@ -8,8 +8,8 @@ print('AAPL Profit Margins: ', aapl_info['profitMargins'])
 print('AAPL Gross Margins: ', aapl_info['grossMargins'])
 print('AAPL Day High: ', aapl_info['dayHigh'])
 
-#aapl_history_max = aapl.history(period='max')
-#aapl_history_max.head()
+aapl_history_max = aapl.history(period='max')
+print(aapl_history_max.head())
 
-aapl_history_1m = aapl.history(start="2024-10-01", end="2024-10-15", interval="2m")
-print(aapl_history_1m.head())
+# aapl_history_1m = aapl.history(start="2024-10-01", end="2024-10-15", interval="2m")
+# print(aapl_history_1m.head())
