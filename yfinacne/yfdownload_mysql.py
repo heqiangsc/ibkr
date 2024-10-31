cd7e53f9d477d0262266597d4e51818e94445f7c
heqiang <heqiang@kuainiugroup.com>
2024-10-31 18:53:26 +0800

    yfinance

--- yfinacne/yfdownload_mysql.py
+++ yfinacne/yfdownload_mysql.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 import mysql.connector
 
 
-engine = create_engine("mysql+mysqlconnector://root:R3OzbedF!wi!@47.254.66.136:3306/yfinance?charset=utf8mb4")
+engine = create_engine("mysql+mysqlconnector://root:R3OzbedF!wi!@47.254.66.136:31321/yfinance?charset=utf8mb4")
 aapl = yf.Ticker('AAPL')
 #获取股票的history数据
 aapl_history_2m = aapl.history(start="2024-10-01", end="2024-10-15", interval="2m")
