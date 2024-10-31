46e086a8f55666839e994a9e4d84d7af4f4996d4
heqiang <heqiang@kuainiugroup.com>
2024-10-31 14:35:54 +0800

    yfinance

--- yfinacne/yfdownload.py
+++ yfinacne/yfdownload.py
@@ -11,5 +11,10 @@ print('AAPL Day High: ', aapl_info['dayHigh'])
 aapl_history_max = aapl.history(period='max')
 print(aapl_history_max.head())
 
+#获取股票的history数据
 aapl_history_1m = aapl.history(start="2024-10-01", end="2024-10-15", interval="2m")
 print(aapl_history_1m.head())
+
+#获取股票的分红和扩股数据
+aapl_actions = aapl.actions
+print(aapl_actions.head())
