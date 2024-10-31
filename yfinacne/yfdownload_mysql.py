3a5beaa06a12ffcdca804153df1af307ad69ff9b
heqiang <heqiang@kuainiugroup.com>
2024-10-31 19:13:36 +0800

    yfinance

--- yfinacne/yfdownload_mysql.py
+++ yfinacne/yfdownload_mysql.py
@@ -6,6 +6,6 @@ import mysql.connector
 engine = create_engine("mysql+mysqlconnector://root:R3OzbedF!wi!@47.254.66.136:31321/yfinance?charset=utf8")
 aapl = yf.Ticker('AAPL')
 #获取股票的history数据
-aapl_history_2m = aapl.history(start="2024-10-01", end="2024-10-15", interval="2m")
-aapl_history_2m.to_sql("aapl_history_2m", engine, index=False)
+aapl_history_2m = aapl.history(start="2024-10-01", end="2024-10-30", interval="2m")
+aapl_history_2m.to_sql("aapl_history_2m", engine)
 print(aapl_history_2m.head())
